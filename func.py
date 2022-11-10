f2542112b284e1bfaba3611a5e27963b473197de
Denis Shaymardanov <d9655352866@gmail.com>
2022-11-10 11:36:30 +0500

    work is done

--- func.py
+++ func.py
@@ -1,8 +1,4 @@
-import view
-
-def get_add():
-    list = ['Фамилию: ','Имя: ','номер телефона: ','коментарий']
-    data = view.get_input(list)
+def add_contact(data):
     with open('people.csv', 'a', encoding = 'UTF-8') as file:
         file.writelines(
             f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
@@ -10,38 +6,32 @@ def get_add():
         file.writelines(
             f'{data[0]} {data[1]} {data[2]} {data[3]} \n')
 
-def get_see():
+def get_contacts():
     with open('people.csv', 'r', encoding = 'UTF-8') as file:
-        list = []
-        list = ''.join(file.readlines())
-        print(list)
+        data = []
+        data = file.readlines()
 
-def get_find():
-    with open('people.csv', 'r', encoding = 'UTF-8') as file:
-        data = []
-        data = file.readlines()
-        text = input('Введите слово для поиска: \n')
-        for i in range(len(data)):
-            data[i] = ''.join(data[i])
-            row = []
-            row = data[i]
-            if text in row:
-                print(f'N {i+1} : {row}')
-        
-def get_del():
-    with open('people.csv', 'r', encoding = 'UTF-8') as file:
-        data = []
-        data = file.readlines()
-        print('-'*100)
-        for i in range(len(data)):
-            print(f'№ {i+1} |{data[i]}')
-    number = int(input('Введите номер контакта для удаления: '))
+    return data
+
+def delete_contact(data, number):
     n = number-1
     data.pop(n)
-    print('Вы удалили контакт')
-    for i in range(len(data)):
-            print(f'№ {i+1} |{data[i]}')
+ 
     with open('people.txt', 'w', encoding = 'UTF-8') as file:
         file.writelines(data)
     with open('people.csv', 'w', encoding = 'UTF-8') as file:
         file.writelines(data)
+
+def find_contact(word):
+    result = []
+    with open('people.csv', 'r', encoding = 'UTF-8') as file:
+        data = []
+        data = file.readlines()
+        for i in range(len(data)):
+            data[i] = ''.join(data[i])
+            row = []
+            row = data[i]
+            if word in row:
+                result.append(f'N {i+1} : {row}')
+
+    return result
